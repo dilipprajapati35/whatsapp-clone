62abb10657416f23150a8bb5f6966bca31cf263d
dilipprajapati35 <128807627+dilipprajapati35@users.noreply.github.com>
2025-01-21 13:22:44 +0530

    added initial structure with splash screen

---
+++ lib/pages/splash.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/welcome');
+    });
+    super.initState();
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/logo.png', height: 100,),
+      ),
+    );
+  }
+}
